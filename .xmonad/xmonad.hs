bb211a50b16abc5c5a82ea1f4fed537b371c7401
Morten Brøns-Pedersen <f@ntast.dk>
2015-12-16 08:55:08 +0100

    trying out google inbox -- i *really* should move away from google's services before they own me...

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -166,7 +166,8 @@ myTopicConfig = TopicConfig
        [ ("im", safeSpawn myTerminal ["-e", "ssh", "lolbox.pwnies.dk", "-t", "screen", "-DR", "irc"])
        -- [ ("im", term)
        , ("web", browser "")
-       , ("organise", appBrowser "http://gmail.com" >>
+       -- , ("organise", appBrowser "http://gmail.com" >>
+       , ("organise", appBrowser "http://inbox.google.com" >>
                       appBrowser "http://calendar.google.com")
        , ("multimedia", appBrowser "https://soundcloud.com/explore/hardcore%2Btechno")
        , ("procrastination", newBrowser
